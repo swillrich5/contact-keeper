23eaebd8b603d4d828d3122cfc44a6636937597a
swillrich5 <swillrich@gmail.com>
2022-04-23 20:43:31 -0500

    finished saving user to db

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
+const bcrypt = require('bcryptjs');
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
+const { restart } = require('nodemon');
 
 const User = require('../models/User');
 
@@ -13,12 +15,33 @@ router.post('/',
         check('email', 'Please include a valid email').isEmail(),
         check('password', 'Please enter a password with six or more characters').isLength({ min: 6 })
     ],
-    (req, res) => {
+    async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array()})
         }
-        res.send("passed");
+    const { name, email, password } = req.body;
+
+    try {
+        let user = await User.findOne({email: email});
+        if (user) {
+            return res.status(400).json({msg: 'User already exists'});
+        }
+        // if user doesn't already exist, create the new user
+        user = new User({
+            name: name ,
+            email: email,
+            password: password
+        });
+        // salt the password and encrypt it
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+        await user.save();
+        res.send('User Saved');
+    } catch (err) {
+        console.error(err.message);
+        res.stat(500).send("Server Error");
+    }
 });
 
 module.exports = router;
